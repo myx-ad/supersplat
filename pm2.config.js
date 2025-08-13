3779022259191e2586c567335cba5c6444fea392
Kiril Trenkov <k.trenkov@myxrobotics.com>
2025-08-13 15:49:35 +0300

    rework 3

--- pm2.config.js
+++ pm2.config.js
@@ -4,22 +4,15 @@ module.exports = {
             name: 'supersplat-viewer',
             script: 'npm',
             args: 'run serve',
-
+            interpreter: 'none', // important when using npm as "script"
             watch: false,
             source_map_support: true,
             combine_logs: true,
             time: true,
             instances: 1,
-            env: {
-                // Everything else is loaded from the .env file
-                NODE_ENV: 'production',
-            },
-            env_staging: {
-                NODE_ENV: 'staging',
-            },
-            env_development: {
-                NODE_ENV: 'dev',
-            },
+            env: { NODE_ENV: 'production' },
+            env_staging: { NODE_ENV: 'staging' },
+            env_development: { NODE_ENV: 'dev' },
         },
     ],
     deploy: {
@@ -28,23 +21,20 @@ module.exports = {
             host: 'maverick',
             ref: 'origin/master',
             repo: 'git@github.com:myx-ad/supersplat.git',
-            path: '/home/platform/supersplat/',
-            'pre-setup': '',
-            'post-setup': '',
-            'pre-deploy-local': '',
+            path: '/home/platform/supersplat', // drop trailing slash
+            ssh_options: 'ForwardAgent=yes',
             'post-deploy':
-                'npm install; export NODE_ENV=production; npm run build && pm2 reload pm2.config.js',
+                'npm ci --omit=dev && npm run build && pm2 reload pm2.config.js --env production --update-env',
         },
         staging: {
             user: 'user',
             host: '10.24.16.73',
             ref: 'origin/integration',
             repo: 'git@github.com:myx-ad/supersplat.git',
-            path: '/home/user/supersplat/',
-            'pre-setup': '',
-            'post-setup': '',
-            'pre-deploy-local': '',
-            'post-deploy': 'npm install; npm run build && pm2 reload pm2.config.js --env development',
+            path: '/home/user/supersplat',
+            ssh_options: 'ForwardAgent=yes',
+            'post-deploy':
+                'npm ci && npm run build && pm2 reload pm2.config.js --env development --update-env',
         },
     },
 };
